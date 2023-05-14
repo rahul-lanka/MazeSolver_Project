c374d8244394628e08d35dade01a0079926f8e95
Rahul Lanka <122143777+rahul-lanka@users.noreply.github.com>
2023-05-15 00:02:02 +0530

    Files Added Via Upload

---
+++ MazeSolver_Project.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mazesolver_project;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MazeSolver_Project extends JFrame {
+
+    private int maze[][]=
+    {{1,1,1,1,1,1,1,1,1,1,1,1,1},
+     {1,0,1,0,1,0,1,0,0,0,0,0,1},
+     {1,0,1,0,0,0,1,0,1,1,1,0,1},
+     {1,0,1,1,1,1,1,0,0,0,0,0,1},
+     {1,0,0,1,0,0,0,0,1,1,1,0,1},
+     {1,0,1,0,1,1,1,0,1,0,0,0,1},
+     {1,0,1,0,1,0,0,0,1,1,1,0,1},
+     {1,0,1,0,1,1,1,0,1,0,1,0,1},
+     {1,0,0,0,0,0,0,0,0,0,1,9,1},
+     {1,1,1,1,1,1,1,1,1,1,1,1,1}
+      
+    };
+    public List<Integer> path=new ArrayList<>();
+    public MazeSolver_Project(){
+        setTitle("Maze Solver");
+        setSize(640,480);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DepthFirst.searchPath(maze,1,1,path);
+    }
+    public void paint(Graphics g){
+        g.translate(50,50);
+        
+        for(int i=0;i<maze.length;i++){
+            for(int j=0;j<maze[0].length;j++){
+                Color color;
+                switch(maze[i][j]){
+                    case 1:color=Color.BLACK; break;
+                    case 9:color=Color.RED;   break;
+                    default : color=Color.WHITE; break;
+                }
+                g.setColor(color);
+                g.fillRect(30*j,30*i, 30, 30);
+                g.setColor(Color.RED);
+                g.drawRect(30*j, 30*i, 30, 30);
+            }
+        }
+        for(int i =0;i<path.size();i+=2){
+            int pathx=path.get(i);
+            int pathy=path.get(i+1);
+            
+            System.out.println("X coordinates"+pathx);
+            System.out.println("Y coordinates"+pathy);
+            
+            g.setColor(Color.GREEN);
+            g.fillRect(30*pathx,30*pathy, 20, 20);
+        }
+    }
+    public static void main(String[] args) {
+        MazeSolver_Project view = new MazeSolver_Project();
+        view.setVisible(true);
+    }
+}
